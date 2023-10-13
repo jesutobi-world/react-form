833de3be28e5576695913a659c5ec759fe05a660
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2023-10-13 15:32:21 +0100

    functionalities

--- src/components/AddOns.jsx
+++ src/components/AddOns.jsx
@@ -1,30 +1,31 @@
 
 const AddOns = ({formData, setFormData}) => {
+    
   return (
     <div className="flex flex-col gap-5">
-        <label htmlFor="service" className={`w-full h-auto p-3 flex gap-4 justify-start border border-solid border-coolGray rounded-md items-center ${formData.onlineService === true && 'bg-mangolia border-purpleishBlue'}`} onClick={() => setFormData({ ...formData, onlineService: !formData.onlineService })}>
-            <input type="checkbox" name="" id="service" />
+        <label htmlFor="service" className={`w-full h-auto p-3 flex gap-4 justify-start border border-solid border-coolGray rounded-md items-center ${formData.onlineService === true && 'bg-mangolia border-purpleishBlue'}`} >
+            <input type="checkbox" id="service" onClick={() => setFormData({ ...formData, onlineService: !formData.onlineService })} />
             <div>
                 <div className="text-marineBlue font-semibold text-[13px]">Online service</div>
                 <div className="text-coolGray text-[11px] font-medium">Access to multiplayer games</div>
             </div>
-            <p className="ml-auto text-[12px] font-medium text-purpleishBlue">$1/mo</p>
+            <p className="ml-auto text-[12px] font-medium text-purpleishBlue">+${formData.planSubscription === 'Monthly' ? 1: 10}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
         </label>
-        <label htmlFor="storage" className="w-full h-auto p-3 flex gap-4 justify-start border border-solid border-coolGray rounded-md items-center" onClick={() => setFormData({ ...formData, largeStorage: !formData.largeStorage })}>
-            <input type="checkbox" name="" id="storage" />
+        <label htmlFor="storage" className={`w-full h-auto p-3 flex gap-4 justify-start border border-solid border-coolGray rounded-md items-center ${formData.largeStorage === true && 'bg-mangolia border-purpleishBlue'}`}>
+            <input type="checkbox" id="storage" onClick={() => setFormData({ ...formData, largeStorage: !formData.largeStorage })} />
             <div>
                 <div className="text-marineBlue font-semibold text-[13px]">Larger storage</div>
                 <div className="text-coolGray text-[11px] font-medium">Extra 1TB of cloud save</div>
             </div>
-            <p className="ml-auto text-[12px] font-medium text-purpleishBlue">$2/mo</p>
+            <p className="ml-auto text-[12px] font-medium text-purpleishBlue">+${formData.planSubscription === 'Monthly' ? 2: 20}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
         </label>
-        <label htmlFor="profile" className="w-full h-auto p-3 flex gap-4 justify-start border border-solid border-coolGray rounded-md items-center" onClick={() => setFormData({ ...formData, customizableProfile: !formData.customizableProfile })}>
-            <input type="checkbox" name="" id="profile" />
+        <label htmlFor="profile" className={`w-full h-auto p-3 flex gap-4 justify-start border border-solid border-coolGray rounded-md items-center ${formData.customizableProfile === true && 'bg-mangolia border-purpleishBlue'}`} >
+            <input type="checkbox" id="profile" onClick={() => setFormData({ ...formData, customizableProfile: !formData.customizableProfile })} />
             <div>
                 <div className="text-marineBlue font-semibold text-[13px]">Customizable profile</div>
                 <div className="text-coolGray text-[11px] font-medium">Custom theme on your profile</div>
             </div>
-            <p className="ml-auto text-[12px] font-medium text-purpleishBlue">$2/mo</p>
+            <p className="ml-auto text-[12px] font-medium text-purpleishBlue">+${formData.planSubscription === 'Monthly' ? 2: 20}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
         </label>
     </div>
   )
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,28 +1,49 @@
 
-const Summary = () => {
+const Summary = ({formData, setFormData}) => {
+  let subscriptionPrice = "";
+  if (formData.planName === "Arcade") {
+    subscriptionPrice = formData.planSubscription === "Monthly" ? 9 : 90;
+  } else if (formData.planName === "Advanced") {
+    subscriptionPrice = formData.planSubscription === "Monthly" ? 12 : 120;
+  } else if (formData.planName === "Pro") {
+    subscriptionPrice = formData.planSubscription === "Monthly" ? 15 : 150;
+  }
+
+    // Calculate the individual service costs
+    const onlineServiceCost = formData.onlineService ? (formData.planSubscription === 'Monthly' ? 1 : 10) : 0;
+    const largeStorageCost = formData.largeStorage ? (formData.planSubscription === 'Monthly' ? 2 : 20) : 0;
+    const customizableProfileCost = formData.customizableProfile ? (formData.planSubscription === 'Monthly' ? 2 : 20) : 0;
+  
+    // Calculate the total cost
+    const totalCost = subscriptionPrice + onlineServiceCost + largeStorageCost + customizableProfileCost;
+  
   return (
     <div className="w-full h-auto flex flex-col gap-1">
         <div className="w-full h-auto p-4 bg-mangolia rounded-md flex flex-col gap-3">
             <div className="flex justify-between items-center">
                 <div className="flex justify-between items-start flex-col" >
-                    <p className="text-marineBlue font-semibold text-base">Arcade(Monthly)</p>
+                    <p className="text-marineBlue font-semibold text-base">{formData.planName} ({formData.planSubscription})</p>
                     <a href="" className="text-coolGray text-sm underline">Change</a>
                 </div>
-                <p className="text-marineBlue text-base font-semibold ">$9/mo</p>
+                <p className="text-marineBlue text-base font-semibold ">${subscriptionPrice}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
             </div>
             <hr/>
-            <div className="flex justify-between">
+            <div className={`flex justify-between ${formData.onlineService === true ? 'flex' : 'hidden'}`}>
                 <p className="text-coolGray text-sm">Online Service</p>
-                <p className="text-coolGray text-sm">+$1/mo</p>
+                <p className="text-coolGray text-sm">+${formData.planSubscription === 'Monthly' ? 1: 10}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
             </div>
-            <div className="flex justify-between">
+            <div className={`flex justify-between ${formData.largeStorage === true ? 'flex' : 'hidden'}`}>
                 <p className="text-coolGray text-sm">Larger storage</p>
-                <p className="text-coolGray text-sm">+$2/mo</p>
+                <p className="text-coolGray text-sm">+${formData.planSubscription === 'Monthly' ? 2: 20}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
+            </div>
+            <div className={`flex justify-between ${formData.customizableProfile === true ? 'flex' : 'hidden'}`}>
+                <p className="text-coolGray text-sm">Customizable Profile</p>
+                <p className="text-coolGray text-sm">+${formData.planSubscription === 'Monthly' ? 2: 20}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
             </div>
         </div>
         <div className="flex justify-between p-4">
             <p className="text-coolGray text-sm">Total(per month)</p>
-            <p className="text-purpleishBlue text-lg font-semibold">+$12/mo</p>
+            <p className="text-purpleishBlue text-lg font-semibold">+${totalCost}/{formData.planSubscription === 'Monthly' ? 'mo': 'yr'}</p>
         </div>
     </div>
   )
